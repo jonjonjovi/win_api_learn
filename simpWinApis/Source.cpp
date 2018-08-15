08bed729b6d0997dfa4cd58d7c64cdb0455d39a0
Hartnett, Jonathan <jonathan_hartnett@mcafee.com>
2018-08-15 16:53:27 +0100

    simple apis

--- simpWinApis/Source.cpp
+++ simpWinApis/Source.cpp
@@ -2,7 +2,7 @@
 #include<string>
 #include<windows.h>
 void createdir(std::string);
-
+void cfile(std::string val);
 int main(int argc, char* argv[])
 {
     std::cout << "Entered:"<< argc << std::endl;
@@ -11,6 +11,9 @@ int main(int argc, char* argv[])
     
     
     createdir(argv[1]);
+    std::string cmd = (std::string)argv[1] + (std::string)"\\" + (std::string)argv[2];
+    std::cout << cmd << std::endl;
+    cfile(cmd);
     system("PAUSE");
     return 0;
 }
@@ -20,5 +23,18 @@ void createdir(std::string val)
     LPCSTR lpPathName = val.c_str();
     BOOL val2 = CreateDirectoryA(lpPathName,NULL);
     std::cout << val2 << std::endl;
+
+}
+
+void cfile(std::string val)
+{
+    LPCSTR lpFileName = val.c_str();
+    HANDLE hfile = CreateFileA(lpFileName,
+        GENERIC_READ,
+        0,
+        NULL,
+        CREATE_NEW,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
 
 }
